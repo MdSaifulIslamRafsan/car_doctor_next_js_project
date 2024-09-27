339f5b7bb9b0771daecb6efc4f537b89c4fa6cbd
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-09-27 18:09:50 +0600

    added my bookings api

---
+++ src/app/my-bookings/api/[email]/route.js
@@ -0,0 +1,14 @@
+
+import { connectDb } from "@/lib/connectedDb";
+import { NextResponse } from "next/server";
+
+export const GET = async (request, {params}) => {
+    const db = await connectDb()
+    const bookingsCollection = db.collection('bookings')
+    try {
+        const myBookings = await bookingsCollection.find({email : params.email}).toArray();
+        return NextResponse.json({myBookings})
+    } catch (error) {
+        return NextResponse.json({message : "No Data Found"})
+    }
+}
